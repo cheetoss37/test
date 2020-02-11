336f453be8642d57e15bb60baa73cc6c83891f64
tung nguyen <tungnm72@wru.vn>
2020-02-11 12:08:35 +0700

    update

--- test.js
+++ test.js
@@ -10,11 +10,19 @@ date3.setDate(15);
 var date4= new Date();
 date4.setDate(20);
 var date5= new Date();
-date2.setDate(25);
+date5.setDate(25);
+console.log(date1,date2,date3,date4,date5);
+
 console.log(today)
-
-var fetchDay = [date1, date2, date3, date4, date5];
-console.log(fetchDay);
-
-var same = today.getTime() === date2.getTime();
-var notSame = today.getTime() !== date2.getTime();
+if(JSON.stringify(today) === JSON.stringify(date1) ||
+   JSON.stringify(today) === JSON.stringify(date2) ||
+   JSON.stringify(today) === JSON.stringify(date3) ||
+   JSON.stringify(today) === JSON.stringify(date4) ||
+   JSON.stringify(today) === JSON.stringify(date5) ) {
+       console.log('Hello');
+        /* fetch*/
+        document.write("Fetch")
+   } else {
+        console.log('Bye!') /* show stored api */
+        document.write("Show stored images")
+    }
